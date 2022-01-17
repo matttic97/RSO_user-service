38ed2310072d3722706caa802a1bad08c5028580
Matic Isovski <mi6568@student.uni-lj.si>
2022-01-17 20:00:18 +0100

    local merge

--- api/src/main/java/org/rso/naloga/zapiski/api/v1/UserApplication.java
+++ api/src/main/java/org/rso/naloga/zapiski/api/v1/UserApplication.java
@@ -14,7 +14,7 @@ import javax.ws.rs.core.Application;
 @OpenAPIDefinition(info = @Info(title = "User service API", version = "v1",
         contact = @Contact(email = "mi5658@student.uni-lj.si"),
         license = @License(name = "dev"), description = "API for managing users."),
-        servers = @Server(url = "http://104.45.79.50/users"))
+        servers = @Server(url = "http://104.45.79.50/user"))
 @ApplicationPath("v1")
 public class UserApplication extends Application {
 
