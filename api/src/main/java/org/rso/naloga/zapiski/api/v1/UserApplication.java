5796bcd71615cb4463060f80ca638dc48996d469
Matic Isovski <mi6568@student.uni-lj.si>
2022-01-17 17:45:56 +0100

    k8s

--- api/src/main/java/org/rso/naloga/zapiski/api/v1/UserApplication.java
+++ api/src/main/java/org/rso/naloga/zapiski/api/v1/UserApplication.java
@@ -14,8 +14,8 @@ import javax.ws.rs.core.Application;
 @OpenAPIDefinition(info = @Info(title = "User service API", version = "v1",
         contact = @Contact(email = "mi5658@student.uni-lj.si"),
         license = @License(name = "dev"), description = "API for managing users."),
-        servers = @Server(url = "localhost:8080/"))
-@ApplicationPath("/v1")
+        servers = @Server(url = "http://104.45.79.50/users"))
+@ApplicationPath("v1")
 public class UserApplication extends Application {
 
 }
